72af2990e51a0f267279241dd04641112bf1a564
Paulo Lucas <paulo.lucas.souza@gmail.com>
2022-04-05 11:02:44 -0300

    Added SignIn

--- App.tsx
+++ App.tsx
@@ -19,6 +19,8 @@ import theme from './src/global/styles/theme';
 import {NavigationContainer} from '@react-navigation/native';
 import {AppRoutes} from './src/routes/app.routes';
 
+import {SignIn} from './src/screens/SignIn';
+
 import { Register } from './src/screens/Register';
 
 export default function App() {
@@ -36,7 +38,7 @@ if(!fontsLoaded){
   <ThemeProvider theme={theme}>
     <NavigationContainer>
       <StatusBar barStyle="light-content"/>
-      <AppRoutes />
+      <SignIn />
     </NavigationContainer>
   </ThemeProvider>
 
